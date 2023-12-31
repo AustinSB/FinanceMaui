f484b65a392c53259e14315f4a14ce90816a9a30
Austin Briere <austinbriere93@gmail.com>
2023-12-30 23:47:30 -0500

    Add project files.

---
+++ FinanceMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
